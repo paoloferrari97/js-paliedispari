22dfa0ea0062543314f2eda6c8c8cca800aeb9b8
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-29 16:07:20 +0200

    Inizio il primo esercizio (pari e dispari)

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+//Pari e Dispari:
+//L'utente sceglie pari o dispari
+var pari_dispari_utente = "";
+do {
+    pari_dispari_utente = prompt("Scegli pari o dispari?").toLowerCase();
+} while (pari_dispari_utente != "pari" && pari_dispari_utente != "dispari");
+
+//L’utente inserisce un numero da 1 a 5.
+var numero_utente = 0;
+do {
+    numero_utente = Number(prompt("Scegli un numero tra 1 e 5!"));
+} while (numero_utente < 1 || numero_utente > 5 || isNaN(numero_utente));
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function numeroRandom() {
+    var numero_random = Math.ceil(Math.random() * 5);
+    return numero_random;
+}
+
+console.log(numeroRandom());
+
+//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
