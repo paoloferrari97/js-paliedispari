c8bb2223df69aecf48955a361dd76718ef1fde6f
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-29 16:12:05 +0200

    Creo funzione per esercizio pari e dispari

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 //Pari e Dispari:
+//Funzione per numero random tra 1 e 5
+function numeroRandom() {
+    var numero_random = Math.ceil(Math.random() * 5);
+    return numero_random;
+}
+
 //L'utente sceglie pari o dispari
 var pari_dispari_utente = "";
 do {
@@ -8,16 +14,12 @@ do {
 //L’utente inserisce un numero da 1 a 5.
 var numero_utente = 0;
 do {
-    numero_utente = Number(prompt("Scegli un numero tra 1 e 5!"));
+    numero_utente = parseInt(prompt("Scegli un numero tra 1 e 5!"));
 } while (numero_utente < 1 || numero_utente > 5 || isNaN(numero_utente));
 
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function numeroRandom() {
-    var numero_random = Math.ceil(Math.random() * 5);
-    return numero_random;
-}
-
-console.log(numeroRandom());
+var numero_pc = numeroRandom();
+console.log(numero_pc);
 
 //Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
